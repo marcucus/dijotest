4624ba41e90fb22a5cfff070d3b3f94355d8d11b
Adrien Marques <adrien.marques@etu.unilim.fr>
2023-01-31 20:38:17 +0100

    Fix Bug not found

--- src/components/info.tsx
+++ src/components/info.tsx
@@ -52,7 +52,7 @@ return (
                 </h2>
                 <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
                     <div className="inline-flex rounded-md shadow">
-                        <a href="/" className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-700 px-5 py-3 text-base font-medium text-white hover:bg-red-800">Par ici !</a>
+                        <a href="/activite" className="inline-flex items-center justify-center rounded-md border border-transparent bg-red-700 px-5 py-3 text-base font-medium text-white hover:bg-red-800">Par ici !</a>
                     </div>
                 </div>
             </div>
